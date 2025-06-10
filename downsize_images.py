0e74044176c6bae77cd638cb8a4e3509f0553f78
shruti-cpp <shrutijha2115@gmail.com>
2025-06-10 07:32:43 +0530

    Update downsize_images.py

--- downsize_images.py
+++ downsize_images.py
@@ -1,17 +1,17 @@
 import os
 from PIL import Image
 
-# === CONFIG ===
+
 INPUT_FOLDER = "input_images"
 OUTPUT_FOLDER = "output_images"
 MAX_SIZE = (800, 800)  # Resize to fit in 800x800 box
 
-# === Create Output Folder ===
+
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 success_count = 0
 fail_list = []
-# === Resize Logic ===
+# resizing
 for filename in os.listdir(INPUT_FOLDER):
     if filename.lower().endswith((".jpg", ".jpeg", ".png", ".webp")):
         input_path = os.path.join(INPUT_FOLDER, filename)
@@ -21,15 +21,15 @@ for filename in os.listdir(INPUT_FOLDER):
             img.thumbnail(MAX_SIZE)
             img.save(output_path)
 
-        # Get file sizes
+        # filesize
         original_size = os.path.getsize(input_path)
         new_size = os.path.getsize(output_path)
 
-        # Convert to KB
+        # Kb
         original_kb = original_size / 1024
         new_kb = new_size / 1024
 
-        # Print size info
+    
         print(f"{filename} downsized from {original_kb:.2f} KB to {new_kb:.2f} KB "
               f"({(new_kb / original_kb) * 100:.1f}% of original)")
         success_count += 1
